0f725c1fbc7731ad129695930fc31ae3dda37c4e
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-12 18:00:58 +0500

    Add files via upload

---
+++ variable_basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "enter a first number   ";
+		cin >> a;
+		cout << "enter a second number    ";
+		cin>>b;
+		cout << "a=" << a << "b=" << b << endl;
+		return 0;
+}
