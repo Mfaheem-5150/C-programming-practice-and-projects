c02070eb3a67a1624f1b26d9fea3bbf9ec6c608e
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-12 13:17:01 +0500

    Add files via upload

---
+++ record.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	int reg_no = 1009;
+	int age = 20;
+	string name = "Faheem";
+	cout << "Registration no: " << reg_no << endl;
+	cout << "Name: " << name << endl;
+	cout << "Age: " << age << endl;
+	cout << "University: Quaid-i-Azam University";
+
+	return 0;
+}
