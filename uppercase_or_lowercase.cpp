cd86c5dc8536f8953806238de4c230554eb0f71a
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-13 18:24:52 +0500

    Add files via upload

---
+++ uppercase_or_lowercase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char ch;
+	cout << "Enter character: ";
+	cin >> ch;
+	if (ch >= 'A' && ch <= 'Z')
+	{
+		cout << "Uppercase letter.";
+	}
+	else if (ch >= 'a' && ch <= 'z')
+	{
+		cout << "Lowercase letter";
+    }
+	else {
+		cout << "Not an alphabet";
+	}
+	
+	return 0;
+}
