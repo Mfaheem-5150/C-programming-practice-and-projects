971813ec7ec89594ae01802827f8a7fa87de8d0a
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-15 18:19:36 +0500

    Add files via upload

---
+++ reverse_a_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num, reverse = 0;
+	cout << "Enter a number : ";
+	cin >> num;
+	while (num != 0)
+	{
+		reverse = reverse * 10 + num % 10;
+		num = num / 10;
+	}
+	cout << "Reverse = " << reverse << endl;
+	return 0;
+}
