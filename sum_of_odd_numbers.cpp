a8a76af05744e87ae51b4d5c0707c7174f0acc9a
Muhammad Faheem <muhammadfaheem9986@gmail.com>
2026-09-15 18:07:40 +0500

    Add files via upload

---
+++ sum_of_odd_numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num, sum = 0;
+	cout << "Enter ending number: ";
+	cin >> num;
+
+	for (int i = 1; i <= num; i++)
+	{
+		if (i % 2 != 0)
+			sum = sum + i;
+	}
+	cout << "Sum = " << sum;
+	return 0;
+}
